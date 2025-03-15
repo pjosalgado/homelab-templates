d259d78eeaad074292cd6261d345eb65da93579e
Paulo Salgado <pjosalgado@gmail.com>
2025-03-14 23:31:06 -0300

    Files updated to new pattern. Update LICENSE.

--- Backup/duplicati/send-discord-message.sh
+++ Backup/duplicati/send-discord-message.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-curl -s -X POST -d "{\"content\":\"${DUPLICATI__PARSED_RESULT} on ${DUPLICATI__OPERATIONNAME} of ${DUPLICATI__backup_name}\"}" -H "Content-Type: application/json" $DISCORD_WEBHOOK
+if [ "$DUPLICATI__PARSED_RESULT" != "Success" ]; then
+    curl -s -X POST -d "{\"content\":\"${DUPLICATI__PARSED_RESULT} on ${DUPLICATI__OPERATIONNAME} of [${DUPLICATI__backup_name}](http://$DOMAIN_NAME:8200/ngax/index.html#/log)\"}" -H "Content-Type: application/json" $DISCORD_WEBHOOK_SUCCESS_CHANNEL
+else
+    curl -s -X POST -d "{\"content\":\"${DUPLICATI__PARSED_RESULT} on ${DUPLICATI__OPERATIONNAME} of [${DUPLICATI__backup_name}](http://$DOMAIN_NAME:8200/ngax/index.html#/log)\"}" -H "Content-Type: application/json" $DISCORD_WEBHOOK_OTHERS_CHANNEL
+fi
 exit 0
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Getting started
   - Replace the environment variables (started with _$_) with the desired values before using the scripts.
-    - Recommended the usage of the [direnv](https://direnv.net/) plugin to automate this process.
+    - [direnv](https://direnv.net/) recommended to automate this process.
